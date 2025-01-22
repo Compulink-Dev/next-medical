76d833c28863b8f32438a5a1debf0d32e7cc749a
Compulink-Dev <devcompulink@gmail.com>
2025-01-22 17:06:46 +0200

    main

---
+++ app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const AdminPage = async () => {
+
+  return (
+    <div className="mx-auto flex max-w-7xl flex-col space-y-14">
+
+      <main className="admin-main pt-6">
+        <section className="w-full space-y-4">
+          <h1 className="header">Welcome 👋</h1>
+          <p className="text-dark-700">
+            Dashboard
+          </p>
+        </section>
+
+
+      </main>
+    </div>
+  );
+};
+
+export default AdminPage;
