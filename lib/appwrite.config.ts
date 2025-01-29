673be42d9661f1926e7aeb33feab717820b92e09
Compulink-Dev <devcompulink@gmail.com>
2025-01-29 16:55:06 +0200

    main

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -1,9 +1,9 @@
 import * as sdk from 'node-appwrite'
 
 export const {
-    NEXT_PUBLIC_APPWRITE_PROJECT_ID: PROJECT_ID,
-    NEXT_PUBLIC_APPWRITE_ENDPOINT: ENDPOINT,
-    NEXT_PUBLIC_APPWRITE_API_KEY: API_KEY,
+    NEXT_PUBLIC_PROJECT_ID: PROJECT_ID,
+    NEXT_PUBLIC_ENDPOINT: ENDPOINT,
+    NEXT_PUBLIC_API_KEY: API_KEY,
     NEXT_PUBLIC_DATABASE_ID: DATABASE_ID,
     PATIENT_COLLECTION_ID: PATIENT_COLLECTION_ID,
     DOCTOR_COLLECTION_ID: DOCTOR_COLLECTION_ID,
@@ -30,19 +30,19 @@ console.log({
 });
 
 
-console.log("Appwrite Endpoint:", process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || "Not defined");
-console.log("Appwrite Project ID:", process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || "Not defined");
+console.log("Appwrite Endpoint:", process.env.NEXT_PUBLIC_ENDPOINT || "Not defined");
+console.log("Appwrite Project ID:", process.env.NEXT_PUBLIC_PROJECT_ID || "Not defined");
 
-if (!process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || !process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID) {
+if (!process.env.NEXT_PUBLIC_ENDPOINT || !process.env.NEXT_PUBLIC_PROJECT_ID) {
     throw new Error("Missing Appwrite configuration in environment variables.");
 }
 
 const client = new sdk.Client()
 
 client
-    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!)
-    .setKey(process.env.NEXT_PUBLIC_APPWRITE_API_KEY!)
+    .setEndpoint(process.env.NEXT_PUBLIC_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_PROJECT_ID!)
+    .setKey(process.env.NEXT_PUBLIC_API_KEY!)
 
 export const account = new sdk.Account(client)
 export const databases = new sdk.Databases(client)
